a90950a55b6384cb696c057f9614a95782c230b4
Rob <robsc801@gmail.com>
2021-09-02 15:32:27 +1200

    destructure props

--- src/index.js
+++ src/index.js
@@ -44,11 +44,12 @@ function BookList() {
   )
 }
 const Book = (props) => {
+  const {img, title, author} = props
   return (
     <article className='book'>
-      <img src={props.img} alt='book' />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt='book' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   )
 }
