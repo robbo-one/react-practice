bb75a308623869fda10e32e90360eba5fa0e5c47
Rob <robsc801@gmail.com>
2021-09-02 13:15:31 +1200

    adding props examples

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-
-//CSS
-import './index.css'
-
 // Always return JSX, you always need to return something! even if it's just an empty <div>
 
 // JSX Rules
@@ -12,32 +6,47 @@ import './index.css'
 // use camelCase for html attribute
 // className instead of class
 // close every Element
-// formatting
+// formattingimport React from 'react'
+
+import ReactDom from 'react-dom'
+
+//CSS
+import './index.css'
+
+//setup vars
+const author = 'Mark Thunder'
+const title = "Dee's Big Nuts"
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/61HtUDSnCoL._AC_UL200_SR200,200_.jpg'
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
+      <Book job='developer' />
+      <Book title='random title' number={22}/>
     </section>
   )
 }
-const author = "Mark Thunder"
-const Book = () => {
-  const title = "Dee's Big Nuts"
+const Book = (props) => {
   return (
     <article className='book'>
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/61HtUDSnCoL._AC_UL200_SR200,200_.jpg'
-        alt=''
-      />
+      <img src={img} alt='book' />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
-      {/* <p>{let x = 6}</p> */} 
-      {/* we cannot have a statement here, however if we have an expression everything will work as shown below */}
-      <p>{6 + 6}</p>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+    <p>{props.number}</p>
     </article>
   )
 }
+
+
+//  <p>{let x = 6}</p> 
+
+//  we cannot have a statement here, however if we have an expression everything will work as shown below 
+
+//   <p>{6 + 6}</p> 
+
 
 //const Author = () => {
 //return (
