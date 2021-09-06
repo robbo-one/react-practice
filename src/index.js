faeba143114a57fa89188878cf46c1388e30263e
Rob <robsc801@gmail.com>
2021-09-07 11:46:32 +1200

    add onMouseOver example

--- src/index.js
+++ src/index.js
@@ -52,16 +52,22 @@ function BookList() {
 }
 const Book = ({ img, title, author }) => {
   // attribute, eventHandler
-// onClick, onMouseOver
-const clickHandler = () => {
-  alert('hello world')
-}
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e) 
+    console.log(e.target)
+    alert('hello world')
+  }
   return (
-    <article className='book'>
+    <article className='book' onMouseOver={()=>{
+      console.log(title)
+    }}>
       <img src={img} alt='book' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>reference example</button>
+      <button type='button' onClick={clickHandler}>
+        reference example
+      </button>
     </article>
   )
 }
